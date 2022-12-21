745f02dc3b0215d160a3f6ea18dbf87ea02e0501
Santiago Mena <santimenasa@gmail.com>
2022-12-21 17:07:08 -0300

    create the routes to the get by id

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import Header from "./components/Header";
-import "./index.css"
+import "./index.css";
 import { Route, Routes } from "react-router-dom";
 import React from "react";
 import Home from "./components/Home";
-import AddBird from "./components/AddBird"
-import AboutUs from "./components/AboutUs"
+import AddBird from "./components/AddBird";
+import AboutUs from "./components/AboutUs";
 import Birds from "./components/bird/Birds";
-import BirdDetail from "./components/bird/BirdDetail"
+import BirdDetail from "./components/bird/BirdDetail";
 function App() {
   return (
     <React.Fragment>
@@ -14,13 +14,15 @@ function App() {
         <Header />
       </header>
       <main>
-<Routes>
-  <Route  path="/" element={<Home />} exact/>
-  <Route  path="/add" element={<AddBird />} exact/>
-  <Route  path="/about" element={<AboutUs />} exact/>
- <Route  path="/birds" element={<Birds />}/> 
- <Route path="/birds:id" element={<BirdDetail />} exact />
-</Routes>
+        <Routes>
+          <Route>
+          <Route path="/" element={<Home />} exact />
+          <Route path="/add" element={<AddBird />} exact />
+          <Route path="/about" element={<AboutUs />} exact />
+          <Route path="/birds" element={<Birds />} />
+          <Route path="/birds/:bid" element={<BirdDetail />} exact />
+          </Route>
+        </Routes>
       </main>
     </React.Fragment>
   );
