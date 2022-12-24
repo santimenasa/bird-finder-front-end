ce397ca60cd044eb2a2e027d92ae745c677fcd8b
Santiago Mena <santimenasa@gmail.com>
2022-12-24 20:01:01 -0300

    Search bar

--- src/components/bird/Birds.jsx
+++ src/components/bird/Birds.jsx
@@ -10,6 +10,60 @@ const fetchHandler = async () => {
 };
 
 const Birds = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [birds, setBirds] = useState();
+  useEffect(() => {
+    fetchHandler().then((data) => setBirds(data.birds));
+  }, []);
+  console.log(birds);
+
+  return (
+    <div className="birds">
+     <span className="input-span"> 
+      
+       <input
+       className="input"
+        type={"text"}
+        placeholder={`Search Bird`}
+        onChange={(event) => setSearchTerm(event.target.value)}
+      />
+       </span>
+      <ul>
+        
+        {birds &&
+          birds.filter((val) => {
+            if (!searchTerm ) {
+              return val
+            }else if (val.species.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase())) {
+              return val
+            }
+          })
+          .map((bird, i) => (
+            <li  key={i}>
+              <Bird bird={bird} />
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Birds;
+
+
+
+/* import axios from "axios";
+import React from "react";
+import Bird from "./Bird";
+import "./bird.css"
+import { useEffect, useState } from "react";
+const URL = "http://localhost:5000/birds";
+
+const fetchHandler = async () => {
+  return await axios.get(URL).then((res) => res.data);
+};
+
+const Birds = () => {
   const [birds, setBirds] = useState();
   useEffect(() => {
     fetchHandler().then((data) => setBirds(data.birds));
@@ -32,9 +86,4 @@ const Birds = () => {
 };
 
 export default Birds;
-/* 
-1) hago la funcion fetch para traer la data del back al fron
-2)armo el useState hook
-3)armo el ueEffect hook 
-4) dentro del useeffect indico que mi estado sea lo que traigo del fetch
-*/
+ */
